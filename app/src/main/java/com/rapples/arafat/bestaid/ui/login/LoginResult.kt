3b6aa25f83e6ac0410b00514bfd707a8746a73fd
blue dream <aryan.it08@gmail.com>
2020-04-10 23:26:01 +0600

    initial commit

---
+++ app/src/main/java/com/rapples/arafat/bestaid/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.rapples.arafat.bestaid.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
---
+++ app/src/main/java/com/rapples/arafat/bestaid/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.rapples.arafat.bestaid.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.rapples.arafat.bestaid.data.LoginDataSource
+import com.rapples.arafat.bestaid.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                    loginRepository = LoginRepository(
+                            dataSource = LoginDataSource()
+                    )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
